597b2b59899688a9acf2e8f9628e30d0c15e418f
Donaven Bruce <91450415+DonavenJB@users.noreply.github.com>
2022-03-31 15:52:00 -0700

    Add shared package entry point

---
+++ packages/shared/src/index.ts
@@ -0,0 +1,8 @@
+// @empiryu/shared — single source of truth. Types now, formulas later. No I/O.
+
+export * from './domain/world.js';
+export * from './domain/resources.js';
+export * from './domain/troops.js';
+
+// Contract version.
+export const SHARED_VERSION = '0.0.0';
