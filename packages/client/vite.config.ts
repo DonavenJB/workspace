f06f670a4a1781c0f90fd95a6b92a2f4bc71eb13
Donaven Bruce <91450415+DonavenJB@users.noreply.github.com>
2022-04-09 09:41:00 -0700

    Add Vite config for client

---
+++ packages/client/vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite';
+
+// Dev server. /api proxies to the game server.
+export default defineConfig({
+  server: {
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+});
